7ee0022f0e06f2d2b895f263998ba29392a7790b
Jórdan Luiz Bisato <jordanbisato@hotmail.com>
2026-03-12 18:53:42 -0300

    fix: (Plugins) remove unused repository

--- plugins/salesforce-miaw-android-53.js
+++ plugins/salesforce-miaw-android-53.js
@@ -12,13 +12,10 @@
 const {
   withProjectBuildGradle,
   withAppBuildGradle,
-  withGradleProperties,
-  withAndroidManifest,
-  withDangerousMod,
   createRunOncePlugin,
-} = require('@expo/config-plugins');
-const fs = require('fs');
-const path = require('path');
+} = require("@expo/config-plugins");
+const fs = require("fs");
+const path = require("path");
 
 // ========================================
 // 1. Project Build Gradle (android/build.gradle)
@@ -31,20 +28,23 @@ function withAndroidProjectBuildGradle(config) {
     // 1.1 Add repositories Maven to Salesforce
     // Fix to insert after default repositories (google, mavenCentral, jitpack)
     const salesforceRepos = `
-        maven { url 'https://s3.amazonaws.com/salesforcesos.com/android/maven/release' }
         maven { url "https://s3.amazonaws.com/inapp.salesforce.com/public/android" }`;
 
-    if (
-      !contents.includes('salesforcesos.com/android/maven/release') ||
-      !contents.includes('inapp.salesforce.com')
-    ) {
-      const jitpackRegex = /maven\s*{\s*url\s*['"]https:\/\/www\.jitpack\.io['"]\s*}/;
+    if (!contents.includes("inapp.salesforce.com")) {
+      const jitpackRegex =
+        /maven\s*{\s*url\s*['"]https:\/\/www\.jitpack\.io['"]\s*}/;
       const mavenCentralRegex = /mavenCentral\(\)/;
 
       if (contents.match(jitpackRegex)) {
-        contents = contents.replace(jitpackRegex, (match) => `${match}${salesforceRepos}`);
+        contents = contents.replace(
+          jitpackRegex,
+          (match) => `${match}${salesforceRepos}`,
+        );
       } else if (contents.match(mavenCentralRegex)) {
-        contents = contents.replace(mavenCentralRegex, (match) => `${match}${salesforceRepos}`);
+        contents = contents.replace(
+          mavenCentralRegex,
+          (match) => `${match}${salesforceRepos}`,
+        );
       } else {
         contents = contents.replace(
           /allprojects\s*{\s*repositories\s*{/,
@@ -60,16 +60,19 @@ function withAndroidProjectBuildGradle(config) {
       /classpath\(['"]org\.jetbrains\.kotlin:kotlin-gradle-plugin:.*['"]\)/;
 
     if (contents.match(kotlinGradlePluginRegex)) {
-      console.log('🔄 Updating Kotlin Gradle Plugin to 2.2.10...');
+      console.log("🔄 Updating Kotlin Gradle Plugin to 2.2.10...");
       contents = contents.replace(kotlinGradlePluginRegex, kotlinPluginLine);
     } else {
-      console.log('➕ Updating Kotlin Gradle Plugin to 2.2.10...');
+      console.log("➕ Updating Kotlin Gradle Plugin to 2.2.10...");
       const dependenciesBlockRegex =
         /(buildscript\s*{\s*repositories\s*{[^}]*}\s*dependencies\s*{)/;
       if (contents.match(dependenciesBlockRegex)) {
-        contents = contents.replace(dependenciesBlockRegex, `$1\n${kotlinPluginLine}`);
+        contents = contents.replace(
+          dependenciesBlockRegex,
+          `$1\n${kotlinPluginLine}`,
+        );
       } else {
-        console.warn('Error when trying to add Kotlin Gradle Plugin.');
+        console.warn("Error when trying to add Kotlin Gradle Plugin.");
       }
     }
 
@@ -87,9 +90,9 @@ function withAndroidAppBuildGradle(config) {
     let contents = config.modResults.contents;
 
     // 2.4 Adicionar packagingOptions
-    if (!contents.includes('META-INF/versions/9/OSGI-INF/MANIFEST.MF')) {
+    if (!contents.includes("META-INF/versions/9/OSGI-INF/MANIFEST.MF")) {
       contents = contents.replace(/android\s*{[\s\S]*?}/m, (match) => {
-        if (match.includes('packagingOptions')) {
+        if (match.includes("packagingOptions")) {
           return match.replace(
             /packagingOptions\s*{([\s\S]*?)}/m,
             `packagingOptions {
@@ -129,6 +132,6 @@ function withSalesforceAndroid(config) {
 // Exportar como run-once plugin
 module.exports = createRunOncePlugin(
   withSalesforceAndroid,
-  'salesforce-miaw-android-53',
-  '1.0.0',
+  "salesforce-miaw-android-53",
+  "1.0.0",
 );
--- plugins/salesforce-miaw-android-54.js
+++ plugins/salesforce-miaw-android-54.js
@@ -12,13 +12,10 @@
 const {
   withProjectBuildGradle,
   withAppBuildGradle,
-  withGradleProperties,
-  withAndroidManifest,
-  withDangerousMod,
   createRunOncePlugin,
-} = require('@expo/config-plugins');
-const fs = require('fs');
-const path = require('path');
+} = require("@expo/config-plugins");
+const fs = require("fs");
+const path = require("path");
 
 // ========================================
 // 1. Project Build Gradle (android/build.gradle)
@@ -31,20 +28,23 @@ function withAndroidProjectBuildGradle(config) {
     // 1.1 Add repositories Maven to Salesforce
     // Fix to insert after default repositories (google, mavenCentral, jitpack)
     const salesforceRepos = `
-        maven { url 'https://s3.amazonaws.com/salesforcesos.com/android/maven/release' }
         maven { url "https://s3.amazonaws.com/inapp.salesforce.com/public/android" }`;
 
-    if (
-      !contents.includes('salesforcesos.com/android/maven/release') ||
-      !contents.includes('inapp.salesforce.com')
-    ) {
-      const jitpackRegex = /maven\s*{\s*url\s*['"]https:\/\/www\.jitpack\.io['"]\s*}/;
+    if (!contents.includes("inapp.salesforce.com")) {
+      const jitpackRegex =
+        /maven\s*{\s*url\s*['"]https:\/\/www\.jitpack\.io['"]\s*}/;
       const mavenCentralRegex = /mavenCentral\(\)/;
 
       if (contents.match(jitpackRegex)) {
-        contents = contents.replace(jitpackRegex, (match) => `${match}${salesforceRepos}`);
+        contents = contents.replace(
+          jitpackRegex,
+          (match) => `${match}${salesforceRepos}`,
+        );
       } else if (contents.match(mavenCentralRegex)) {
-        contents = contents.replace(mavenCentralRegex, (match) => `${match}${salesforceRepos}`);
+        contents = contents.replace(
+          mavenCentralRegex,
+          (match) => `${match}${salesforceRepos}`,
+        );
       } else {
         contents = contents.replace(
           /allprojects\s*{\s*repositories\s*{/,
@@ -67,9 +67,9 @@ function withAndroidAppBuildGradle(config) {
     let contents = config.modResults.contents;
 
     // 2.4 Adicionar packagingOptions
-    if (!contents.includes('META-INF/versions/9/OSGI-INF/MANIFEST.MF')) {
+    if (!contents.includes("META-INF/versions/9/OSGI-INF/MANIFEST.MF")) {
       contents = contents.replace(/android\s*{[\s\S]*?}/m, (match) => {
-        if (match.includes('packagingOptions')) {
+        if (match.includes("packagingOptions")) {
           return match.replace(
             /packagingOptions\s*{([\s\S]*?)}/m,
             `packagingOptions {
@@ -110,6 +110,6 @@ function withSalesforceAndroid(config) {
 // Exportar como run-once plugin
 module.exports = createRunOncePlugin(
   withSalesforceAndroid,
-  'salesforce-miaw-android-54',
-  '1.0.0',
+  "salesforce-miaw-android-54",
+  "1.0.0",
 );
